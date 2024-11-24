22942a84e427cf38a0843decfc1aa436c25dc7a7
Josiah <josiahrbernard@gmail.com>
2024-11-23 22:33:42 -0500

    -

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -43,7 +43,7 @@ void Environment::drawLine(Point start, Point end) {
         setPen(true);
 
         //Calculate the total distance and determine how many steps to take
-        double step_size = 0.2;
+        double step_size = 0.5;
         double distance = sqrt(pow(end.x - start.x, 2) + pow(end.y - start.y, 2));
         int steps = distance / step_size;
 
@@ -57,7 +57,7 @@ void Environment::drawLine(Point start, Point end) {
             }
 
             //Sleep for a short duration to slow down the drawing
-            rclcpp::Rate rate(5);
+            rclcpp::Rate rate(2.5);
             rate.sleep();
         }
 
