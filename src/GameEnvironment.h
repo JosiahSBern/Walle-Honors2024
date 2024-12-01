614d8ea99b6a3bc892224f9f668105748430e327
Josiah <josiahrbernard@gmail.com>
2024-12-01 07:15:26 -0500

    unit

--- src/GameEnvironment.h
+++ src/GameEnvironment.h
@@ -12,7 +12,8 @@ private:
 public:
     GameEnvironment(rclcpp::Node::SharedPtr node, const std::string& turtle_name);
     void drawGame();
-    void drawWalls() override;
+    void drawWalls();
+    void drawBins()
 };
 
 #endif  // GAME_ENVIRONMENT_H
