804bd6a39c56b1322cd1b2ef23cbb3b4f2a2c185
Josiah <josiahrbernard@gmail.com>
2024-12-01 06:54:24 -0500

    unit

--- src/GameEnvironment.cpp
+++ src/GameEnvironment.cpp
@@ -19,10 +19,27 @@ GameEnvironment::GameEnvironment(rclcpp::Node::SharedPtr node) : Environment(nod
 
 // Draw bins
 void GameEnvironment::drawBins() {
+    const double binWidth = 2.0;
+    const double binHeight = 1.5;
+    const double bottomBoxHeight = 2.0;
+
     RCLCPP_INFO(node_->get_logger(), "Drawing bins...");
-    drawRectangle(binPositions[0], {3.0, 7.5});
-    drawRectangle(binPositions[1], {6.0, 7.5});
-    drawRectangle(binPositions[2], {9.0, 7.5});
+
+    // Draw top bins
+    for (size_t i = 0; i < binPositions.size(); ++i) {
+        Point binBottomRight = {binPositions[i].x + binWidth, binPositions[i].y - binHeight};
+        drawRectangle(binPositions[i], binBottomRight);
+        RCLCPP_INFO(node_->get_logger(), "Bin %zu: TopLeft (%f, %f), BottomRight (%f, %f)", 
+                    i + 1, binPositions[i].x, binPositions[i].y, binBottomRight.x, binBottomRight.y);
+    }
+
+    // Draw the large box at the bottom
+    Point bottomBoxTopLeft = {1.0, 3.0};
+    Point bottomBoxBottomRight = {10.0, 3.0 - bottomBoxHeight};
+    drawRectangle(bottomBoxTopLeft, bottomBoxBottomRight);
+
+    RCLCPP_INFO(node_->get_logger(), "Bottom box: TopLeft (%f, %f), BottomRight (%f, %f)",
+                bottomBoxTopLeft.x, bottomBoxTopLeft.y, bottomBoxBottomRight.x, bottomBoxBottomRight.y);
 }
 
 // Draw the game
@@ -34,7 +51,15 @@ void GameEnvironment::drawGame() {
 
 // Draw walls
 void GameEnvironment::drawWalls() {
-    Point topLeft = {1.0, 1.0};
-    Point bottomRight = {10.0, 10.0};
+    const double WALL_LEFT = 1.0;
+    const double WALL_RIGHT = 10.0;
+    const double WALL_TOP = 10.0;
+    const double WALL_BOTTOM = 1.0;
+
+    Point topLeft = {WALL_LEFT, WALL_TOP};
+    Point bottomRight = {WALL_RIGHT, WALL_BOTTOM};
     drawRectangle(topLeft, bottomRight);
+
+    RCLCPP_INFO(node_->get_logger(), "Walls drawn: TopLeft (%f, %f), BottomRight (%f, %f)",
+                topLeft.x, topLeft.y, bottomRight.x, bottomRight.y);
 }
