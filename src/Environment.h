c19310803361b83b0205cafb3c8c13b2c81f7f77
Josiah <josiahrbernard@gmail.com>
2024-12-01 08:50:01 -0500

    UNIT

--- src/Environment.h
+++ src/Environment.h
@@ -14,15 +14,12 @@ protected:
     rclcpp::Node::SharedPtr node_;                   // ROS2 node for managing the environment
     rclcpp::Client<turtlesim::srv::SetPen>::SharedPtr pen_client_;  // Pen service client
     rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr teleport_client_;  // Teleport service client
-    Point exit;
     std::string direction;
 
 public:
     virtual ~Environment() = default;
     Environment(rclcpp::Node::SharedPtr node, const std::string& turtle_name);
     virtual void drawWalls() = 0;
-    virtual void setExit(double x, double y, const std::string& direction);
-    Point getExit();
     void drawLine(Point start, Point end, bool pen_state, int r, int g, int b, int width);
     void drawRectangle(Point topLeft, Point bottomRight, int r, int g, int b);
     void setPen(bool pen_state, int r, int g , int b, int width = 2);
