85b51f9bf6cb04dd4c82dffb0f0de2c9dff703cf
Josiah <josiahrbernard@gmail.com>
2024-12-03 09:18:29 -0500

    -

--- src/Turtle.h
+++ src/Turtle.h
@@ -5,7 +5,6 @@
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
-#include <cmath>
 
 class Turtle {
 protected:
@@ -16,20 +15,25 @@ protected:
     std::shared_ptr<rclcpp::Node> node_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
 
-    // Calculate the Euclidean distance between two points
-    double calculateDistance(const Point& p1, const Point& p2) const;
-
 public:
-    Turtle(std::shared_ptr<rclcpp::Node> node, const std::string& name, double radius);
-    virtual ~Turtle();
+    Turtle(std::shared_ptr<rclcpp::Node> node, const std::string& name, double radius)
+        : node_(node), name(name), radius(radius), position({0.0, 0.0}) {
+        twist_pub_ = node_->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 1);
+    }
 
     virtual void move() = 0;
     virtual void renderTurtle() = 0;
+    virtual ~Turtle() = default;
 
-    bool checkCollision(const Point& other, double otherRadius) const;
+    bool checkCollision(const Point& other, double otherRadius) {
+        double dx = position.x - other.x;
+        double dy = position.y - other.y;
+        double distance = std::sqrt(dx * dx + dy * dy);
+        return distance <= (radius + otherRadius);
+    }
 
-    Point getPosition() const;
-    void setPosition(const Point& newPosition);
+    Point getPosition() const { return position; }
+    void setPosition(const Point& newPosition) { position = newPosition; }
 };
 
-#endif  // TURTLE_H
+#endif
