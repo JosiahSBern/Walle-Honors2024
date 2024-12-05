dccdef08776e646b50e667171ab5f6f7e4ab86dc
Josiah <josiahrbernard@gmail.com>
2024-12-05 09:38:26 -0500

    -

--- src/TeleopMain.cpp
+++ src/TeleopMain.cpp
@@ -1,6 +1,5 @@
 #include "TeleopTurtle.h"
 #include <rclcpp/rclcpp.hpp>
-s
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<rclcpp::Node>("teleop_turtle_node");
